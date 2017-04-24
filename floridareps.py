c99ae9baa2faab23b9699445cabeedd76372185b
nicoledan <nicole_k_dan@hotmail.com>
2017-04-24 00:16:03 -0400

    new commit

--- floridareps.py
+++ floridareps.py
@@ -1,30 +1,28 @@
 from urllib.request import Request, urlopen
 from bs4 import BeautifulSoup
 import csv
+# open new file for writing
 html = urlopen("https://www.govtrack.us/congress/members/FL")
-bsObj = BeautifulSoup(html, "html5lib")
+bsObj = BeautifulSoup(html, "html.parser")
 
 
-reps = []
-content = []
+csvfile = open("floridareps.csv", 'w')
+c = csv.writer(csvfile)
+# write the header row for CSV file
+c.writerow(['name', 'party'])
 
-def getDetails(reps):
-    global content
-    for value in reps:
-        bsObj = BeatifulSoup(html, "html.parser")
-        names = bsObj.findAll("p", {"class":"moc"}).get_text,
-        parties = bsObj.findAll("div", {"class":"info"}).get_text
+def get_member_details(bsObj):
+    members = bsObj.findAll("div", {"class":"member"})
+    for member in members:
+        row = []
+        name = member.find("p", {"class":"moc"}).get_text()
+        party = member.find("div", {"class":"info"}).find("div").get_text()
+        row.append( name )
+        row.append( party )
+        c.writerow( row )
 
+get_member_details(bsObj)
 
+# write your for-loop here - including c.writerow( row ) in the loop
 
-def CSV(content):
-    filename = "floridareps.csv"
-    with open(filename, 'w') as output_file:
-        fieldnames = [ 'names', 'parties' ]
-        writer = csv.DictWriter(output_file, fieldnames=fieldnames)
-        writer.writeheader()
-        writer.writerows(content)
-
-
-getDetails(reps)
-CSV(content)
+csvfile.close()
--- politifact.py
+++ politifact.py
@@ -1,10 +1,33 @@
 from urllib.request import Request, urlopen
 from bs4 import BeautifulSoup
+import csv
 html = urlopen("http://www.politifact.com/personalities/matt-gaetz/")
 bsObj = BeautifulSoup(html, "html5lib")
 
+csvfile = open("politifact.csv", 'w')
+c = csv.writer(csvfile)
+# write the header row for CSV file
+c.writerow(['representative', 'true', 'mostly true', 'half true', 'mostly false', 'false', 'pants on fire'])
 
-rulings = bsObj.findAll("div", {"class":"peepDetailRuling"})
+def get_names (bsObj):
+    names = bsObj.find("div", {"class":"statement__source"})
+    for name in names:
+        row = []
+        representative = name.find("div", {"class":"statement__source"}).get_text()
+        row.append( representative )
+        c.writerow( row )
 
-for ruling in rulings:
-    print(ruling.get_text())
+def get_ruling_details (bsObj):
+    rulings = bsObj.findAll("li", {"class":"chartlist__item"})
+    for ruling in rulings:
+        row = []
+        true_statement = ruling.find("span", {"class":"chartlist_count"}).get_text() [1::6]
+        mostly_true = ruling.find("span", {"class":"chartlist_count"}).get_text() [2::6]
+        half_true = ruling.find("span", {"class":"chartlist_count"}).get_text() [3::6]
+        mostly_false = ruling.find("span", {"class":"chartlist_count"}).get_text() [4::6]
+        false_statement = ruling.find("span", {"class":"chartlist_count"}).get_text() [5::6]
+        pants_on_fire = ruling.find("span", {"class":"chartlist_count"}).get_text() [6::6]
+        c.writerow( row )
+
+get_names(bsObj)
+get_ruling_details(bsObj)
